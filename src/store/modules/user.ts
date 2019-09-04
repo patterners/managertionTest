68a2a78dfde8ea87c976d148b4d7f4dff50209b0
dreamSeekerYu <3024637102@qq.com>
2019-09-04 15:29:19 +0800

    first demo

---
+++ src/store/modules/user.ts
@@ -0,0 +1,29 @@
+//import { LoginForm } from './../../types/index';
+import {observable, action} from 'mobx'
+import {login} from '@/service/index'
+//import {HttpInfo, HttpType, LoginForm} from '../../types/index'
+
+//console.log(LoginForm)
+interface LoginForm{
+    user_name: string,
+    user_pwd: string
+}
+class User{
+    @observable isLogin: boolean = false;
+
+    @action async login(form: LoginForm): Promise<any>{
+        let result: any = await login(form);
+        console.log('result...', result);
+        return result.code;
+
+        // if (form.user_name === 'chenmanjie' && form.user_pwd === 'Chenmanjie123!'){
+        //     this.isLogin = true;
+        //     return {code: 1}
+        // }else{
+        //     this.isLogin = false;
+        //     return {code: 0}
+        // }
+    }
+}
+
+export default User;
